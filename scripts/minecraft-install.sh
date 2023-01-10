d286e8cc33d86b2ceff58d39b5cd71fe57b86db1
Joel Holmes <Holmes89@gmail.com>
2023-01-09 20:49:49 -0500

    Update minecraft-install.sh

--- scripts/minecraft-install.sh
+++ scripts/minecraft-install.sh
@@ -2,17 +2,17 @@
 
 # set -e, -u, -x, -o pipefail
 
-apt update -y
-apt install curl wget unzip grep screen openssl -y
+sudo apt update -y
+sudo apt install curl wget unzip grep screen openssl -y
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb -O libssl1.1.deb
-dpkg -i libssl1.1.deb
+sudo dpkg -i libssl1.1.deb
 rm libssl1.1.deb
-useradd -m mcserver
+sudo useradd -m mcserver
 
-mkdir -p /home/mcserver/minecraft_bedrock
+sudo mkdir -p /home/mcserver/minecraft_bedrock
 DOWNLOAD_URL=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" https://minecraft.net/en-us/download/server/bedrock/ |  grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*')
-wget $DOWNLOAD_URL -O /home/mcserver/minecraft_bedrock/bedrock-server.zip
-unzip /home/mcserver/minecraft_bedrock/bedrock-server.zip -d /home/mcserver/minecraft_bedrock/
-rm /home/mcserver/minecraft_bedrock/bedrock-server.zip
-chown -R mcserver: /home/mcserver/
+sudo wget $DOWNLOAD_URL -O /home/mcserver/minecraft_bedrock/bedrock-server.zip
+sudo unzip /home/mcserver/minecraft_bedrock/bedrock-server.zip -d /home/mcserver/minecraft_bedrock/
+sudo rm /home/mcserver/minecraft_bedrock/bedrock-server.zip
+sudo chown -R mcserver: /home/mcserver/
 # cp server.properties /home/mcserver/minecraft_bedrock/server.properties

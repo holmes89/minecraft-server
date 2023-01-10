51677377c1d7986fdbf8dc50cbbfc4c218c3858e
Joel Holmes <Holmes89@gmail.com>
2023-01-09 21:30:25 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -6,23 +6,6 @@ module "minecraft_label" {
   enabled    = module.this.enabled
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-
 resource "aws_security_group" "instance" {
   name        = module.minecraft_label.id
   description = "Allow all inbound traffic"
@@ -37,19 +20,11 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_instance" "minecraft" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = var.ami
   instance_type = "t3.micro"
   security_groups = [module.minecraft_label.id]
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt-get update
-              wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.51.01.zip
-              mkdir minecraft
-              unzip bedrock-server-*.zip -d ./minecraft/
-              cd minecraft
-              LD_LIBRARY_PATH=.
-              export LD_LIBRARY_PATH
-              chmod +rwx bedrock_server
               ./bedrock_server
                 EOF
   
